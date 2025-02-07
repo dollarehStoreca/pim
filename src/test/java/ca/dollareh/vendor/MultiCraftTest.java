111b90864476942aa9f5270f1806398d2e4e0d53
Rajadurai <k.rajadurai@gmail.com>
2025-02-07 14:13:12 +0530

    Product creation json

--- src/test/java/ca/dollareh/vendor/MultiCraftTest.java
+++ src/test/java/ca/dollareh/vendor/MultiCraftTest.java
@@ -1,15 +1,8 @@
 package ca.dollareh.vendor;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.util.List;
-
-import org.apache.hc.core5.http.NameValuePair;
-import org.apache.hc.core5.net.URIBuilder;
-import org.apache.hc.core5.net.URLEncodedUtils;
 import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
 
 class MultiCraftTest {
 
