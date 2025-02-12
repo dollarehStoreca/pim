6334d92e06702e4c1eca838235c82b09e3866bfe
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-02-12 11:51:07 +0530

    Changes

--- src/main/java/ca/dollareh/core/model/Product.java
+++ src/main/java/ca/dollareh/core/model/Product.java
@@ -1,5 +1,5 @@
 package ca.dollareh.core.model;
 
-public record Product(String code, String title, String description) {
+public record Product(String code, String title, String description, float price, String[] imageUrls) {
     
 }
