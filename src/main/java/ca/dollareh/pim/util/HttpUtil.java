725b948b736dddb5beea79c4e2f711dbb0900196
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-03-18 10:52:59 +0530

    Added HTTP Util

---
+++ src/main/java/ca/dollareh/pim/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package ca.dollareh.pim.util;
+
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
+
+import java.net.URISyntaxException;
+
+public class HttpUtil {
+    public static String getRequestParameter(final String url,final String param) throws URISyntaxException {
+        return new URIBuilder(url)
+                .getQueryParams()
+                .stream()
+                .filter(nameValuePair -> nameValuePair.getName().equals("code"))
+                .findFirst()
+                .map(NameValuePair::getValue)
+                .orElse(null);
+    }
+}
