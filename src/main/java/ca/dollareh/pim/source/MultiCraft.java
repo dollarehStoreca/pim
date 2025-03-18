cd3dab828189a0f9974729c4cdbc5b5a178ffb47
Sobhan369 <sobhanbabukarri@gmail.com>
2025-03-18 11:54:36 +0530

    added

--- src/main/java/ca/dollareh/pim/source/MultiCraft.java
+++ src/main/java/ca/dollareh/pim/source/MultiCraft.java
@@ -1,13 +1,17 @@
 package ca.dollareh.pim.source;
 
 import ca.dollareh.pim.model.Product;
+import ca.dollareh.pim.util.HttpUtil;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 public class MultiCraft extends ProductSource {
@@ -37,6 +41,20 @@ public class MultiCraft extends ProductSource {
     @Override
     protected void browse() throws IOException, URISyntaxException {
         logger.info("Browsing Multicraft");
+        Document document = getHTMLDocument("/en/brand/");
+
+        Element ulElement = document.selectFirst("ul.brandsList");
+        Elements liElements = ulElement.children();
+
+        liElements.stream().parallel().forEach(liElement -> {
+            try {
+                browse(HttpUtil.getRequestParameter(liElement.selectFirst("a").attr("href"), "code"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
@@ -45,10 +63,33 @@ public class MultiCraft extends ProductSource {
     }
 
     private void browse(final String category) throws IOException, URISyntaxException {
+        logger.info("Browsing brands {}", category);
+        Document subBrandDocument = getHTMLDocument("/en/brand/subbrands?code=" + category);
+        Element brandUlElements = subBrandDocument.getElementById("skusCards");
+        Elements brandLi = brandUlElements.children();
 
+        brandLi.stream().parallel().forEach(brandLiElement -> {
+//            logger.info("reading product info....");
+            Elements imageElements = brandLiElement.select("img.skuSummary-img");
+            String[] imgUrls = imageElements.stream().parallel().map(imageElement -> {
+                return imageElements.attr("src");
+            }).toArray(String[]::new);
+
+            logger.info("Product title for {}, is {}", category, Arrays.toString(imgUrls));
+            Product product = new Product(
+                    brandLiElement.selectFirst("div.skuSummary-top-brief").selectFirst("p").text(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+        });
     }
 
-    private Document getHTMLDocument(Connection session, final String url) throws IOException {
+    private Document getHTMLDocument(final String url) throws IOException {
         return session.newRequest(BASE_URL + url).get();
     }
 }
