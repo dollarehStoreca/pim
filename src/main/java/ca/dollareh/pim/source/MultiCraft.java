8cce8685bafd57632ea0e7c26baf67442736fe31
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-03-18 22:28:16 +0530

    Download Products

--- src/main/java/ca/dollareh/pim/source/MultiCraft.java
+++ src/main/java/ca/dollareh/pim/source/MultiCraft.java
@@ -13,6 +13,7 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -73,9 +74,6 @@ public class MultiCraft extends ProductSource {
             });
         }
 
-        // Get Sub Categories
-
-
 
     }
 
@@ -84,7 +82,7 @@ public class MultiCraft extends ProductSource {
         return null;
     }
 
-    private void browse(final List<String> categories) throws IOException {
+    private void browse(final List<String> categories) throws IOException, URISyntaxException {
         logger.info("Browsing brand {}", categories);
         Document subBrandDocument = getHTMLDocument("/en/brand/subbrands?code=" + categories.getLast());
 
@@ -94,8 +92,11 @@ public class MultiCraft extends ProductSource {
 
         skusEls.stream().parallel().forEach(skusEl -> {
             String productCode = skusEl.selectFirst(".summary-id").text().trim();
-
-            logger.info("Product Found {}", productCode);
+            try {
+                onProductDiscovery(categories, getProduct(productCode));
+            } catch (IOException e) {
+                logger.error("Product Not Obtained {}", productCode);
+            }
         });
 
 
@@ -103,13 +104,85 @@ public class MultiCraft extends ProductSource {
 
         Elements brandsEls = subBrandDocument.select("ul.brandsList>li>a");
 
-        if(brandsEls.isEmpty()) {
-            System.out.println("Empty");
-        } else {
-            for (Element brandsAnchorEl : brandsEls) {
-                System.out.println(brandsAnchorEl);
+        for (Element brandsAnchorEl : brandsEls) {
+            String code = HttpUtil.getRequestParameter(brandsAnchorEl.attr("href"), "code");
+            List<String> subCategoryPath = new ArrayList<>(categories);
+            subCategoryPath.add(code);
+            browse(subCategoryPath);
+        }
+
+    }
+
+    /**
+     * Get Product from Multicraft.
+     * @param productCode
+     * @return Product
+     * @throws IOException
+     */
+    private Product getProduct(final String productCode) throws IOException {
+
+        Document doc = getHTMLDocument("/en/brand/sku?id=" + productCode);
+
+        Elements imageEls = doc.select("#img-previews>li>img");
+
+        String[] imageUrls = new String[imageEls.size()];
+
+        for (int i = 0; i < imageEls.size(); i++) {
+            imageUrls[i] = imageEls.get(i).attr("src").split("\\?")[0];
+        }
+
+        Elements fieldEls = doc.select("div.details-brief > .row");
+
+        float price = 0;
+        float discount = 0;
+
+        Long upc = null;
+
+        Integer invertyQty = 0;
+
+        for (Element fieldEl : fieldEls) {
+            if (fieldEl.selectFirst(".hdr").text().equals("unit price")) {
+
+                String priceText = fieldEl
+                        .selectFirst(".vlu")
+                        .text()
+                        .replace("$", "");
+                if (priceText.contains(" ")) {
+                    String[] spli = priceText.split(" ");
+                    price = Float.parseFloat(spli[0]);
+                    discount = Float.parseFloat(spli[1]);
+                } else {
+                    price = Float.parseFloat(priceText);
+                }
+
+            } else if (fieldEl.selectFirst(".hdr").text().equals("pack")) {
+                String priceText = fieldEl
+                        .selectFirst(".vlu")
+                        .text();
+
+                invertyQty = Integer.parseInt(priceText);
+
+            } else if (fieldEl.selectFirst(".hdr").text().equals("UPC")) {
+                String upcTxt = fieldEl
+                        .selectFirst(".vlu")
+                        .text();
+                if (!upcTxt.trim().isEmpty()) {
+                    upc = Long.parseLong(upcTxt);
+                }
+
             }
         }
+
+        return new Product(
+                productCode
+                , doc.selectFirst(".details-desc") == null ? null : doc.selectFirst(".details-desc").text()
+                , doc.selectFirst(".details-blurb") == null ? null : doc.selectFirst(".details-blurb").text()
+                , upc
+                , invertyQty
+                , price
+                , discount
+                , imageUrls
+        );
     }
 
     private Document getHTMLDocument(final String url) throws IOException {
