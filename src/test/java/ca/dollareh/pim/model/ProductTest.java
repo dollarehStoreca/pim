49fcd1453fdd01cd4a4dabca5cee211b210edbe7
sathishk <sathishkumar.thiyagarajan@gmail.com>
2025-03-21 10:03:40 +0530

    Invetory Item Set 4

--- src/test/java/ca/dollareh/pim/model/ProductTest.java
+++ src/test/java/ca/dollareh/pim/model/ProductTest.java
@@ -1,11 +1,38 @@
 package ca.dollareh.pim.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
 
     @Test
     void merge() {
+        Product original = new Product("Code1",
+        "Title1",
+        "Description1",
+                1000L,
+        10,
+        1.0f,
+        1.0f,
+        0.5f,
+        null);
+
+        Product transform = new Product(null,
+                "Title2",
+                "Description2",
+                1000L,
+                10,
+                null,
+                2.0f,
+                0.5f,
+                null);
+
+        Product merged = original.merge(transform);
+
+        Assertions.assertEquals(merged.code(), "Code1");
+        Assertions.assertEquals(merged.title(), "Title2");
+
+        // Assertions.assertEquals(merged.price(), 2.0f);
 
     }
 }
