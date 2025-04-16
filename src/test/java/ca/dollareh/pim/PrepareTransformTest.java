c1a5f6857b5f600edf59e52c0adf691a7683a755
Sobhan369 <sobhanbabukarri@gmail.com>
2025-04-16 10:15:14 +0530

    Updated prepareTranformTest to Double the price while transforming

--- src/test/java/ca/dollareh/pim/PrepareTransformTest.java
+++ src/test/java/ca/dollareh/pim/PrepareTransformTest.java
@@ -3,6 +3,8 @@ package ca.dollareh.pim;
 import ca.dollareh.pim.source.Conglom;
 import ca.dollareh.pim.source.Pepperi;
 import ca.dollareh.pim.source.ProductSource;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -25,17 +27,21 @@ class PrepareTransformTest {
 
         for (File jsonFile: jsonFiles) {
             System.out.println(jsonFile.getAbsolutePath());
-
             String fileName = jsonFile.getName();
 
             fileName = fileName.substring(0, fileName.lastIndexOf("-"));
 //            fileName = fileName.substring(0, fileName.lastIndexOf("-"));
 //            fileName = fileName.substring(0, fileName.lastIndexOf("-"));
 
+            final ObjectMapper objectMapper = new ObjectMapper();
+            // Convert to JsonNode
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+            double price = jsonNode.get("price").asDouble() * 2.0d;
+
             File newJsonFile = new File(transformFolder, fileName + ".json");
 
-            if(!newJsonFile.exists()) {
-                Files.writeString(newJsonFile.toPath(), "{\"price\": 10}");
+            if (!newJsonFile.exists()) {
+                Files.writeString(newJsonFile.toPath(), String.format("{\"price\": %s}", price));
             }
         }
     }
