b73724669ee8cf9285c28d544ea23627943a05b8
narayanamurthy <snarayana.murthy86@gmail.com>
2025-04-17 15:58:16 +0530

    Fixed SP, IP image issue for Conglom. added pdf Conglom-Catalogue.pdf

--- src/main/java/ca/dollareh/pim/source/Conglom.java
+++ src/main/java/ca/dollareh/pim/source/Conglom.java
@@ -3,14 +3,15 @@ package ca.dollareh.pim.source;
 import ca.dollareh.pim.model.Product;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.StringUtil;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import technology.tabula.ObjectExtractor;
 import technology.tabula.Page;
 import technology.tabula.Table;
+import technology.tabula.extractors.BasicExtractionAlgorithm;
 import technology.tabula.extractors.SpreadsheetExtractionAlgorithm;
 
 import java.io.File;
@@ -22,6 +23,7 @@ import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -48,6 +50,8 @@ public class Conglom extends ProductSource{
 
     @Override
     protected void browse() throws IOException, URISyntaxException {
+        Map<String, List<String>> imageUrlsByItem = getImageUrlsByItem();
+//        System.exit(0);
 
         Map<String, Float> priceMap = getPriceMap();
 
@@ -72,18 +76,20 @@ public class Conglom extends ProductSource{
 
             if(price != null) {
 
-                String imageURL = row.getCell(4).getStringCellValue().trim().replace(URL, "");
+//                String imageURL = row.getCell(4).getStringCellValue().trim().replace(URL, "");
 
                 // Find Actual Images
-                String imageURLIP = imageURL.replaceFirst("_CASE", "_IP");
-                String imageURLSP = imageURLIP.replaceFirst("_IP", "_SP");
+//                String imageURLIP = imageURL.replaceFirst("_CASE", "_IP");
+//                String imageURLSP = imageURLIP.replaceFirst("_IP", "_SP");
+
 
                 Product product =new Product(code,
                         row.getCell(1).getStringCellValue().trim(),
                         row.getCell(1).getStringCellValue().trim(),null, null,
                         price, price, null,
-                        List.of(imageURLIP,
-                                imageURLSP).toArray(new String[0]));
+                        imageUrlsByItem.get(code).toArray(new String[0]));
+//                        List.of(imageURLIP,
+//                                imageURLSP).toArray(new String[0]));
                 onProductDiscovery(List.of(row.getCell(2).getStringCellValue().trim()),product);
             }
 
@@ -113,13 +119,9 @@ public class Conglom extends ProductSource{
 
                                 price = price.substring(1);
                                 priceMap.put(code, Float.parseFloat(price));
-
-
                             }
-
                         }
                     }
-
                 }
             }
         }
@@ -141,5 +143,46 @@ public class Conglom extends ProductSource{
 
         inputStream.close();
         outputStream.close();
+    }
+
+    private Map<String, List<String>> getImageUrlsByItem() throws IOException {
+
+        Map<String, List<String>> imageUrlMap = new HashMap<>();
+        File file = new File("sample/conglom/Conglom-Catalogue.pdf");
+
+        try (PDDocument document = Loader.loadPDF(file)) {
+            ObjectExtractor extractor = new ObjectExtractor(document);
+            BasicExtractionAlgorithm bea = new BasicExtractionAlgorithm();
+
+            for (int i = 0; i < document.getNumberOfPages(); i++) {
+                Page page = extractor.extract(i + 1);
+                List<Table> tables = bea.extract(page);
+
+                for (Table table : tables) {
+                    int rowCount = table.getRowCount();
+
+                    for (int rowNumber = 2; rowNumber < rowCount; rowNumber++) {
+                        String code = table.getCell(rowNumber, 0).getText().trim();
+
+                        if (!code.isEmpty()) {
+                            String imageUrl = table.getCell(rowNumber, 3).getText().trim().replaceAll(" ", "");
+
+                            /*
+                             * sometimes the column index 3 is not giving image url for few rows, changing col index to 5 gets the image url.
+                             */
+                            if(StringUtil.isBlank(imageUrl) || !imageUrl.startsWith("https")) {
+                                imageUrl = table.getCell(rowNumber, 5).getText().trim().replaceAll(" ", "");
+                            }
+                            if(!StringUtil.isBlank(imageUrl) && imageUrl.startsWith("https") && !imageUrl.contains("_CASE")){
+                                imageUrlMap.computeIfAbsent(code, k -> new ArrayList<>()).add(imageUrl);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        imageUrlMap.values().stream().filter(value -> value.size() > 2).forEach(System.out::println);
+
+        return imageUrlMap;
     }
 }
